9c2eb2b98a5ae9343e8f75f7359df6d87f0e71b1
The73756 <the73756the@gmail.com>
2022-07-14 22:42:16 +0900

    add prototype "add to favorit" btn animation

--- src/App.js
+++ src/App.js
@@ -54,9 +54,29 @@ function App() {
           </article>
           {/* Переписать и сделать как я хочу и вижу. *по нормальному - голая верстка* */}
           <article className="card">
-          <button className="btn-reset carBbtn carBbtnLike">
-            <img width={14} height={12} src="/img/heart.svg" alt="" />
-          </button>
+            <div className="cardLikeBtn">
+              <label className="like">
+                <input type="checkbox" className="checkbox" />
+
+                <div className="like-icon">
+                  <svg viewBox="0 0 32 32" className="below">
+                    <path
+                      d="M23.6 2c-3.363 0-6.258 2.736-7.599 5.594-1.342-2.858-4.237-5.594-7.601-5.594-4.637 0-8.4 3.764-8.4 8.401 0 9.433 9.516 11.906 16.001 21.232 6.13-9.268 15.999-12.1 15.999-21.232 0-4.637-3.763-8.401-8.4-8.401z"
+                      fill="#eae"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                  <svg viewBox="0 0 32 32" className="front">
+                    <path
+                      d="M23.6 2c-3.363 0-6.258 2.736-7.599 5.594-1.342-2.858-4.237-5.594-7.601-5.594-4.637 0-8.4 3.764-8.4 8.401 0 9.433 9.516 11.906 16.001 21.232 6.13-9.268 15.999-12.1 15.999-21.232 0-4.637-3.763-8.401-8.4-8.401z"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                </div>
+              </label>
+            </div>
+          {/* <button className="">
+          </button> */}
           <img width={133} height={112} src="/img/sneakers/2.jpg" alt="Зеленые кроссовки Nike Blazer Mid Suede" />
           <h3 className="cardTitle">Мужские Кроссовки Nike Air Max 270</h3>
           <div className="d-flex justify-between">
