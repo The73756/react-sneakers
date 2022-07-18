eeb892db0f97dfb3fca54ae5f43db4d254f6ba1e
The73756 <the73756the@gmail.com>
2022-07-18 19:14:54 +0900

    update render use mockapi

--- src/App.js
+++ src/App.js
@@ -5,21 +5,8 @@ import Hero from './components/Hero/';
 import Drawer from './components/Drawer';
 import './components/getScrollWidth';
 
-const arr = [
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
-  {name: 'Мужские Кроссовки Nike Air Max 270', price: 12999, imageUrl: '/img/sneakers/2.jpg'},
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
-  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
-  {name: 'Мужские Кроссовки Under Armour Curry 8', price: 15199, imageUrl: '/img/sneakers/5.jpg'},
-  {name: 'Мужские Кроссовки Nike Kyrie 7', price: 11299, imageUrl: '/img/sneakers/6.jpg'},
-  {name: 'Мужские Кроссовки Jordan Air Jordan 11', price: 10799, imageUrl: '/img/sneakers/7.jpg'},
-  {name: 'Мужские Кроссовки Nike LeBron XVIII', price: 16499, imageUrl: '/img/sneakers/8.jpg'},
-  {name: 'Мужские Кроссовки Nike Lebron XVIII Low', price: 13999, imageUrl: '/img/sneakers/9.jpg'},
-  {name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/10.jpg'},
-  {name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8499, imageUrl: '/img/sneakers/11.jpg'},
-  {name: 'Мужские Кроссовки Nike Kyrie Flytrap IV', price: 11299, imageUrl: '/img/sneakers/12.jpg'},
-];
   function App() {
+    const [items, setItems] = React.useState([]);
     const [cartOpened, setCartOpened] = React.useState(false);
 
     const disableScoll = () => {
@@ -30,6 +17,14 @@ const arr = [
       }
     };    
     disableScoll();
+
+    React.useEffect(() => {
+      fetch('https://62d5284e5112e98e4859cd67.mockapi.io/items')
+      .then(res => res.json())
+      .then(json => {
+        setItems(json);
+      });
+    }, []);
 
   return (
     <div className="container">
@@ -46,7 +41,7 @@ const arr = [
             <input type="text" placeholder='Поиск...'/>
           </div>
         </div>
-        <div className="sneakers">{arr.map((obj) => (
+        <div className="sneakers">{items.map((obj) => (
           <Card
           title={obj.name}
           price={obj.price}
